3ebfb6ea2983758cea659164265d7cf26f47ce31
Jeremy Katras <jkatras@fullsail.edu>
2013-10-09 14:58:49 -0400

    sample index.php

--- katras_jeremy/sample/index.php
+++ katras_jeremy/sample/index.php
@@ -3,5 +3,21 @@
 <head><title>iTunes Example</title></head>
 <body>
 <h1>iTunes PHP Example</h1>
+<p>Welcome to PHP.</p>
+<?php
+	$dsn = "mysql:host=127.0.0.1; port=8889;dbname=webapp";
+	$db_user = "root";
+	$db_pass = "root";
+	$db = new \PDO($dsn, $db_user, $db_pass);
+	$statement = $db->prepare("
+		SELECT firstname, lastname, Name
+		FROM users
+	");
+	if($statement->execute()) {
+		$rows = $statement->fetchAll(\PDO::FETCH_ASSOC);
+		var_dump($rows);
+	}
+	
+?>
 </body>
 </html>
