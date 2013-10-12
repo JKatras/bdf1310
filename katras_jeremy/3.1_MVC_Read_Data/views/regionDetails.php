0d00496230e90edd7ad92416a2b7fd0e4ccb8e8c
Jeremy Katras <jkatras@fullsail.edu>
2013-10-11 23:55:55 -0400

    successful views on detail page, possibly correct navigation

--- katras_jeremy/3.1_MVC_Read_Data/views/regionDetails.php
+++ katras_jeremy/3.1_MVC_Read_Data/views/regionDetails.php
@@ -1,9 +1,15 @@
 <?php  
 
+
+$view = new viewModel();
+
+$view->headerView('');
+	
 foreach ($data as $d) {
 	echo "<h4>${d['charName']}</h4>";
 	echo "";
 	echo "<p>House Allegiance: ${d['house']}</p>";
 	}
-	
+
+$view->footerview();
 ?>
