7f881081a560dccddc22806a8498fc7207d79cae
Jeremy Katras <jkatras@fullsail.edu>
2013-10-14 15:15:42 -0400

    authenticate.php setup for login form

---
+++ katras_jeremy/3.3_MVC_User_Login/controllers/authenticate.php
@@ -0,0 +1,34 @@
+<?php  
+session_start();
+include 'models/DB.php';
+include 'models/authModel.php';
+include 'models/viewModel.php';
+
+$model = new authModel();
+$view = new viewModel();
+
+$username = empty($_POST['username']) ? '' : trim($_POST['username']);
+$password = empty($_POST['password']) ? '' : trim($_POST['password']);
+
+$display = 'loginForm.php';
+
+if (!empty($_SESSION['userinfo'])) {
+	$display = 'loginSuccess.php';
+}
+
+if(!empty($username) && !empty($password)) {
+	$user = $model->checkLogin($username, $password);
+	
+	if (is_array($user)) {
+		$display = 'loginSuccess.php';
+		
+		$_SESSION['userInfo'] = $user;
+	}
+}
+
+
+$view->headerView('');
+$view->show($display);
+$view->footerview();
+
+?>
