c7d3a791c11dc8bb0f2d7e56dad5b4181211c3d7
Jeremy Katras <jkatras@fullsail.edu>
2013-10-13 21:14:36 -0400

    loginForm basic html

---
+++ practice/duplicated MVC/views/loginForm.php
@@ -0,0 +1,8 @@
+<?php  
+	echo '<form method="POST" action="">
+		<label for="user">Username</label>
+			<input type="text" name="user" id="iser" maxlength="25" />
+		<label for="pass">Password</label>
+			<input type="password" name="pass" id="pass" maxlength="25" />
+		</form>'
+?>
