70da927fe8b108067c03c3230a0d87e7a43cc8a4
Jeremy Katras <jkatras@fullsail.edu>
2013-10-13 21:31:56 -0400

    auth page basic code; views/includes

--- practice/duplicated MVC/controllers/authenticate.php
+++ practice/duplicated MVC/controllers/authenticate.php
@@ -1,3 +1,14 @@
 <?php  
 
+include 'models/DB.php';
+include 'models/authModel.php';
+include 'models/authView.php';
+
+$model = new authModel();
+$view = new authView();
+
+$view->headerView('');
+$view->formView();
+$view->footerview();
+
 ?>
--- practice/duplicated MVC/views/loginForm.php
+++ practice/duplicated MVC/views/loginForm.php
@@ -1,8 +1,11 @@
-<?php  
+<?php
+	echo '<h2>Member Login</h2>'
+	echo '<h3>Please enter your Username and Password</h3>'  
 	echo '<form method="POST" action="">
 		<label for="user">Username</label>
 			<input type="text" name="user" id="iser" maxlength="25" />
 		<label for="pass">Password</label>
 			<input type="password" name="pass" id="pass" maxlength="25" />
+		<input type="submit" value="Login" />
 		</form>'
 ?>
