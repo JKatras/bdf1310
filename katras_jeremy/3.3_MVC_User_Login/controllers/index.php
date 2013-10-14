aa3e4abf869924b5a5cb2df9a1c51e006e1afbbb
Jeremy Katras <jkatras@fullsail.edu>
2013-10-14 18:32:24 -0400

    add reset to CSS, remove list-style

--- katras_jeremy/3.3_MVC_User_Login/controllers/index.php
+++ katras_jeremy/3.3_MVC_User_Login/controllers/index.php
@@ -3,12 +3,12 @@
 
 include "models/regionModel.php";
 include "models/viewModel.php";
-include "models/authModel.php";
+//include "models/authModel.php";
 
 $model = new regionModel();
 $rows = $model->getRegions();
 $view = new viewModel();
-$auth = new authModel();
+//$auth = new authModel();
 
 $view->show('header.inc');
 
@@ -21,9 +21,9 @@ if(!empty($_GET["action"])){
 	if($_GET["action"]=="login"){
 		$view->show("authenticate.php");
 	}
-	if($_GET["action"]=="logout"){
-		$auth->logout();
-	}
+//	if($_GET["action"]=="logout"){
+//		$auth->logout();
+//	}
 }else {
 	$view->regionListView($rows);
 }
