1311051082236badf9073761fcffdb732e67fac6
Jeremy Katras <jkatras@fullsail.edu>
2013-10-17 21:47:54 -0400

    Started CRUD: new userModel, userCRUD controller

---
+++ practice/duplicated MVC/controllers/models/userModel.php
@@ -0,0 +1,33 @@
+<?php 
+class userModel{
+	public function __construct() {
+	
+	}
+
+	public function getUserInfo($name, $pass) {
+		$db = new DB();
+		$db->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+		$statement = $db->db->prepare("
+			SELECT userId AS id, username AS name
+			FROM users
+			WHERE (username = :name)
+				AND (password = MD5(CONCAT(user_salt,:pass)))
+		");
+		try {
+			if($statement->execute(array(':name' => $name, ':pass' => $pass))) {
+				$rows = $statement->fetchAll(\PDO::FETCH_ASSOC);
+				if (count($rows) === 1) {
+					return $rows[0];
+				}
+			}
+		}
+		catch (\PDOException $e) {
+			echo "There was an error; please try again later";
+			var_dump($e);
+		}
+		return FALSE;
+	} //checkLogin
+
+}
+
+?>
