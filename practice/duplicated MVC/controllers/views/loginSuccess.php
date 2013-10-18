9be95e227bd6a034ee67a9a7ffd399b656aefc4a
Jeremy Katras <jkatras@fullsail.edu>
2013-10-18 18:27:15 -0400

    can't get user 'read' info working

--- practice/duplicated MVC/controllers/views/loginSuccess.php
+++ practice/duplicated MVC/controllers/views/loginSuccess.php
@@ -1,5 +1,8 @@
-<?php  
+<?php
+//foreach ($data as $d){
 	echo "<h2>You were successfully logged in.</h2>";
-	echo "<a href='userCRUD.php'>View User Info</a><br />";
+	echo "<a href=userCRUD.php?action=userInfo&userId={['userId']}>View User Info</a><br />";
 	echo "<a href='index.php?action=logout'>Log Out</a>";
+//}
+
 ?>
