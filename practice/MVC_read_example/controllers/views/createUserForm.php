21f062a591e70fcd300cb74d08fb6f223e92ee57
Jeremy Katras <jkatras@fullsail.edu>
2013-10-19 19:37:42 -0400

    name change, addUser = createUser

--- practice/MVC_read_example/controllers/views/createUserForm.php
+++ practice/MVC_read_example/controllers/views/createUserForm.php
@@ -1,23 +1,23 @@
 <?php  
 	echo '<h2>New Member Registration</h2>';
 	echo '<p>Fields marked with (*) are required</p>';
-	echo '<form method="POST" action="?action=addUser">
-		<label for="username">Username*</label>
+	echo '<form method="POST" action="?action=createUser">
+		<label for="username">Username* </label>
 			<input type="text" name="username" id="username" maxlength="25" />
 			<br />
-		<label for="password">Password*</label>
+		<label for="password">Password* </label>
 			<input type="password" name="password" id="password" maxlength="25" />
 			<br />
-		<label for="email">Email*</label>
+		<label for="email">Email* </label>
 			<input type="email" name="email" id="email" maxlength="30"/>
 			<br />
-		<label for="firstname">First Name</label>
+		<label for="firstname">First Name </label>
 			<input type="text" name="firstname" id="firstname" maxlength="25" />
 			<br />
-		<label for="lastname">Last Name</label>
+		<label for="lastname">Last Name </label>
 			<input type="text" name="lastname" id="lastname" maxlength="25" />
 			<br />
-		<label for="favchar">Favorite Character</label>
+		<label for="favchar">Favorite Character </label>
 			<input type="text" name="favchar" id="favchar" maxlength="40" />
 			<br />
 			<input type="submit" value="Sign Me Up" />
