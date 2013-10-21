8ac3080c125154b05c68b85fc9ecfd0dd23f46e1
Jeremy Katras <jkatras@fullsail.edu>
2013-10-20 23:04:22 -0400

    Revert 3706746..9a247be
    This rolls back to commit 37067468b92dfad89366e4a8ed82be88e468b57c.

--- katras_jeremy/3.5_MVC_CRUD/controllers/userMain.php
+++ katras_jeremy/3.5_MVC_CRUD/controllers/userMain.php
@@ -15,55 +15,48 @@ if(!empty($_GET["action"])) {
 
 	if($_GET["action"]=="authenticate"){
 		$result = $user->authenticate($_POST["username"], $_POST["password"]);
-		if ($result ==array()) {
-			if (count($result)>0) {
-				$view->getView("views/userView.php", $result);
-			}else {
-				echo "<p><b>Please check your login and try again</b></p>";
-				$view->getView("views/loginForm.php");
-			}
-		}else {
-			echo $result;
-			$view->getView("views/loginForm.php");
-		}
+	//	if (count($result)>0) {
+			$view->getView("views/userView.php", $result);
+	//	}else {
+	//		echo "<p><b>Please check your login and try again</b></p>";
+	//		$view->getView("views/loginForm.php");
+	//	}
 	}
-	if ($_GET["action"]=="create") {
+	else if ($_GET["action"]=="create") {
 		$view->getView("views/createUserForm.php");
 	}
 	
 	else if ($_GET["action"]=="createUser") {
-			$result = $user->createUser($_POST["firstname"], $_POST["lastname"],$_POST["username"], $_POST["password"], $_POST["email"], $_POST["favChar"]);
-			if ($result == array()){
-				$view->getView("views/userView.php", $result);
-			}else {
-				echo $result;
-				$view->getView("views/createUserForm.php");
-			}
+		$result = $user->createUser($_POST["firstname"], $_POST["lastname"],$_POST["username"], $_POST["password"], $_POST["email"], $_POST["favChar"]);
+//		$result = $user->getUserDetails($_GET["userId"]);
+		$view->getView("views/userView.php", $result);
+//		header("location: userMain.php");
+//		$view->getView("views/userView.php");
 	}//createUser
 	
-	
-	if( $_GET["action"]=="update"){
+	else if( $_GET["action"]=="update"){
 		$result = $user->getUserDetails($_GET["userId"]);
 		$view->getView("views/updateUserForm.php", $result);
 	}//update
 	
-	if ($_GET["action"]=="updateUser") {
+	else if ($_GET["action"]=="updateUser") {
 		$result = $user->updateUser($_GET["userId"], $_POST["firstname"], $_POST["lastname"],
 		$_POST["email"], $_POST["favChar"]);
-		$view->getView("views/userView.php", $result);
-
+//		if (!empty($result)) {
+			$view->getView("views/userView.php", $result);
+//		}
 		
 	}//updateUser
 	
 	else if ($_GET["action"]=="delete") {
 		$user->deleteUser($_GET["userId"]);
 		header("location: index.php");
-	}//delete
+	}
 	
 	else if ($_GET["action"]=="logout") {
 		$user->logout();
 		header("location: index.php");
-	}//delete
+	}
 	
 }else {
 		$view->getView("views/loginForm.php");
---
+++ practice/MVC_read_example/controllers/models/protector.php
@@ -0,0 +1,8 @@
+<?php  
+if ($_SESSION["loggedin"]==1) {
+}
+
+//else {
+//	header("location: index.php");
+//}
+?>
