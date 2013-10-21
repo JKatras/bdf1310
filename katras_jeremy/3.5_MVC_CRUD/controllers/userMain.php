9a247bebffd1d6cc27bbffa81d03939b2ef793d7
Jeremy Katras <jkatras@fullsail.edu>
2013-10-20 22:44:04 -0400

    userMain can report errors from usersModel

--- katras_jeremy/3.5_MVC_CRUD/controllers/userMain.php
+++ katras_jeremy/3.5_MVC_CRUD/controllers/userMain.php
@@ -15,48 +15,55 @@ if(!empty($_GET["action"])) {
 
 	if($_GET["action"]=="authenticate"){
 		$result = $user->authenticate($_POST["username"], $_POST["password"]);
-	//	if (count($result)>0) {
-			$view->getView("views/userView.php", $result);
-	//	}else {
-	//		echo "<p><b>Please check your login and try again</b></p>";
-	//		$view->getView("views/loginForm.php");
-	//	}
+		if ($result ==array()) {
+			if (count($result)>0) {
+				$view->getView("views/userView.php", $result);
+			}else {
+				echo "<p><b>Please check your login and try again</b></p>";
+				$view->getView("views/loginForm.php");
+			}
+		}else {
+			echo $result;
+			$view->getView("views/loginForm.php");
+		}
 	}
-	else if ($_GET["action"]=="create") {
+	if ($_GET["action"]=="create") {
 		$view->getView("views/createUserForm.php");
 	}
 	
 	else if ($_GET["action"]=="createUser") {
-		$result = $user->createUser($_POST["firstname"], $_POST["lastname"],$_POST["username"], $_POST["password"], $_POST["email"], $_POST["favChar"]);
-//		$result = $user->getUserDetails($_GET["userId"]);
-		$view->getView("views/userView.php", $result);
-//		header("location: userMain.php");
-//		$view->getView("views/userView.php");
+			$result = $user->createUser($_POST["firstname"], $_POST["lastname"],$_POST["username"], $_POST["password"], $_POST["email"], $_POST["favChar"]);
+			if ($result == array()){
+				$view->getView("views/userView.php", $result);
+			}else {
+				echo $result;
+				$view->getView("views/createUserForm.php");
+			}
 	}//createUser
 	
-	else if( $_GET["action"]=="update"){
+	
+	if( $_GET["action"]=="update"){
 		$result = $user->getUserDetails($_GET["userId"]);
 		$view->getView("views/updateUserForm.php", $result);
 	}//update
 	
-	else if ($_GET["action"]=="updateUser") {
+	if ($_GET["action"]=="updateUser") {
 		$result = $user->updateUser($_GET["userId"], $_POST["firstname"], $_POST["lastname"],
 		$_POST["email"], $_POST["favChar"]);
-//		if (!empty($result)) {
-			$view->getView("views/userView.php", $result);
-//		}
+		$view->getView("views/userView.php", $result);
+
 		
 	}//updateUser
 	
 	else if ($_GET["action"]=="delete") {
 		$user->deleteUser($_GET["userId"]);
 		header("location: index.php");
-	}
+	}//delete
 	
 	else if ($_GET["action"]=="logout") {
 		$user->logout();
 		header("location: index.php");
-	}
+	}//delete
 	
 }else {
 		$view->getView("views/loginForm.php");
