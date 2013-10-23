1c953b2171eb13d8a3336dda2a86e61f9687bba8
Jeremy Katras <jkatras@fullsail.edu>
2013-10-22 21:05:05 -0400

    started sample app

---
+++ katras_jeremy/CodeIgniter_sample_app/ci/application/controllers/site.php
@@ -0,0 +1,11 @@
+<?php
+
+class Site extends CI_controller {
+	
+	public function index() {
+		$this->load->view('contact');
+	}
+	
+}
+
+?>
---
+++ katras_jeremy/CodeIgniter_sample_app/ci/application/views/contact.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>Contact Us</title>
+</head>
+
+<body>
+	<h1>Welcome to the Contact Page</h1>
+</body>
+</html>
+
+<?php
+
+
+?>
