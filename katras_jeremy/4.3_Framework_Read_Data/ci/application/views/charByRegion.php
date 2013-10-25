981fbd3bdca346c32b3789b160f32c7cef567c88
Jeremy Katras <jkatras@fullsail.edu>
2013-10-24 20:48:59 -0400

    home links changed

--- katras_jeremy/4.3_Framework_Read_Data/ci/application/views/charByRegion.php
+++ katras_jeremy/4.3_Framework_Read_Data/ci/application/views/charByRegion.php
@@ -1,13 +1,8 @@
 <?php  
-//if (is_array($result)) {
-//
-//
-//foreach ($result as $r) {
-	echo "<h3>Name: </h3>";
-	echo "<h2>${'charName'}</h2>";
-	echo "<h3>House Allegiance: </h3>";
-	echo "<h2>${'house'}</h2>";
-//
-//}
-//}
+//Can't do a foreach here without errors; never recognizes variables sent
+echo "<h3>Name: </h3>";
+echo "<h2>${'charName'}</h2>";
+echo "<h3>House Allegiance: </h3>";
+echo "<h2>${'house'}</h2>";
+
 ?>
